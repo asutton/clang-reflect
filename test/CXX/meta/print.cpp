36ebdead1a6e00c8659d00b098d3ab61405a26b5
Andrew Sutton <andrew.n.sutton@gmail.com>
2018-01-22 11:08:51 -0500

    Add template instantatiation to print test.
    Check to make sure (not in an automated way) that clang isn't emitting
    symbols for the constexpr functions being called.

--- test/CXX/meta/print.cpp
+++ test/CXX/meta/print.cpp
@@ -9,14 +9,24 @@ struct S {
   int m;
 };
 
-immediate void print_members(meta::object x) {
+constexpr int print_members(meta::object x) {
   meta::compiler.print("print members");
   meta::compiler.print(x);
   for (auto m : x)
     meta::compiler.print(m);
+  meta::compiler.print("----------");
+  return 0;
 }
 
+template<typename T>
+constexpr int print_members()
+{
+  return print_members(reflexpr(T));
+}
+
+
 int main(int argc, const char* argv[]) {
-  print_members(reflexpr(S));
+  constexpr int n1 = print_members(reflexpr(S));
+  constexpr int n2 = print_members<S>();
 } 
 
